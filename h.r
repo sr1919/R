1f59086c5ba575af1c58a41fb64c7a988e5928d9
Sergey S. <sr1919@gmail.com>
2012-11-20 16:56:14 +0900

    * par() for plot

--- h.r
+++ h.r
@@ -1,19 +1,35 @@
-#source("ext/plot.depth.r")
-
 load <- function(){
-    t<-read.csv("data/report.csv", header = FALSE, strip.white=TRUE)
-    return( as.matrix(t) )
+    t <- as.matrix( read.csv("data/report.csv", header = FALSE, strip.white=TRUE) )
+    #class( t )
+    #dim( t )
+    #fix( hg )
+    return( t )
 }
 
-#hg<-array( load()->x, dim(x) )
-hg<-load()
-class( hg )
-dim( hg )
-fix( hg )
+run <- function() {
 
-for( i in 1:nrow( hg ) ){
-	png( "img.png", width=400, height=100 )
-    #plot( hg[i,], type"l", ylab="graylevel", xlab="row" )
-    plot( hg[i,], type"l", ylab="", xlab="" )
-    dev.off()
+    hg <- load()
+    mx <- 1.1 * max( hg )
+
+    par( mar = c( 2, 2, 1, 1 ), 
+        mgp = c( 2, .5, 0 ), 
+        cex = 0.7 )
+
+    for( i in 1:nrow( hg ) ){
+        file<-paste( paste("report/img", i, sep="" ), ".png", sep="" )
+
+        png( file, width=500, height=200 )
+        plot( hg[i,], 
+            ylim = c( 0, mx ),
+            type = "s", ylab = "", xlab = "" )
+        grid()
+        dev.off()
+    }
+
+    plot( hg[10,], 
+        ylim = c( 0, mx ),
+        type = "s", ylab = "", xlab = "" )
+        grid()
 }
+
+run()
